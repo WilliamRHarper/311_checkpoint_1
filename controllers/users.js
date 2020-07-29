d080e65d0521460f70be62b6ee75c3777aff421c
WilliamRHarper <Wrharpe1230@gmail.com>
2020-07-28 22:33:30 -0500

    routes, controllers, changes to main index and error handling

---
+++ controllers/users.js
@@ -0,0 +1,56 @@
+const users = require("../data/index");
+// const sampleUsers = require('../data/sampleUser');
+const sampleUser = require("../data/sampleUser");
+// const commentCount = comments.length;
+
+const listUsers = (req, res) => {
+  res.json(users);
+};
+
+const showUser = (req, res) => {
+  const id = req.params.id;
+  const foundUser = users.find((user) => user.id === Number(id));
+  if (!foundUser) {
+    res.status(404).send({ error: "User doesn't exist" });
+  } else {
+    res.json(foundUser);
+  }
+};
+const createUser = (req, res) => {
+  users.push(sampleUser);
+  res.json(sampleUser);
+};
+const updateUser = (req, res) => {
+  const id = req.params.id;
+  const foundUser = users.find((user) => user.id === Number(id));
+  if (!foundUser) {
+    res.status(404).send({ error: "User doesn't exist" });
+  }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === Number(id)) {
+      users[i].name = sampleUser.name;
+    }
+  }
+  res.json(users);
+};
+const deleteUser = (req, res) => {
+  const id = req.params.id;
+  const foundUser = users.find((user) => user.id === Number(id));
+  if (!foundUser) {
+    res.status(400).send({error: "Bad Request" });
+  }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === Number(id)) {
+      users.splice(i, 1);
+    }
+  }
+  res.json(users);
+};
+
+module.exports = {
+  listUsers,
+  showUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 4000;
+const usersRoutes = require('./routes/users');
 
-app.get('/', (req, res) => res.send('default route'))
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(usersRoutes);
+
+app.get("/", (req, res) => res.send("default route"));
 
 app.listen(port, () => {
-  console.log('app is listening on:', port)
-})
+  console.log("app is listening on:", port);
+});
